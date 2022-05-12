372684b3dd185c4710697567f08742ab22c1a59e
jusep1993 <joseantoniolopezbenitez93@gmail.com>
2022-05-12 20:37:35 +0200

    Second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newstring = "";
+    
+    if(num < 0){
+        return "ERROR";
+    }
 
+    for(let i = 0; i < num; i++){
+        newstring += string;
+    }
+
+    return newstring;
 };
 
 // Do not edit below this line
